340b06314fbf4453fc5860714c15c204a658561f
mayi <799278834@qq.com>
2018-03-09 18:21:31 +0800

    2018.3.9_01

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>{{title}}</h1>
+    <h2>My favorite hero is: {{myHero.name}}</h2>
+    <p>Heroes:</p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+    <p *ngIf="heroes.length > 3">There are many heroes!</p>
+    <h1 highlight>{{title}}</h1>
+    <button (click)="onSave($event)">Save</button>
+    <button *ngFor="let hero of heroes" (click)="deleteHero(hero)">{{hero.name}}</button>
+    <form #heroForm (ngSubmit)="onSubmit(heroForm)"> ... </form>
+    <button [disabled]="isUnchanged">Cancel is disabled</button>
+    <h1>{{title}}</h1>
+    <app-heroes></app-heroes>
+`,
+
 })
 export class AppComponent {
   title = 'app';
+  // heroes = ['Windstorm', 'Bombasto', 'Magneta', 'Tornado'];
+  // myHero = this.heroes[0];
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(13, 'Bombasto'),
+    new Hero(15, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+// import { SizerComponent } from './sizer.component';
+import {HighlightDirective} from "./highlight.directive";
+import { HeroesComponent } from './heroes/heroes.component';
+import { TestComponent } from './test/test.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    // SizerComponent,
+    HighlightDirective,
+    HeroesComponent,
+    TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
+
+
 })
+
 export class AppModule { }
